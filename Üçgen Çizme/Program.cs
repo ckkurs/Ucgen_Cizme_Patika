815ceff63c5f43ff944f505664b2e90483cb55d6
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-21 17:21:46 +0300

    Ucgen_Cizme_Patika

---
+++ Üçgen Çizme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Üçgen_Çizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgen Kenar Uzunluğunu Giyiniz: ");
+            int kenaruzunlugu=int.Parse(Console.ReadLine());
+
+            for(int i= 1;i <= kenaruzunlugu;i++)
+            {
+                 for (int j = 1; j <= kenaruzunlugu - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+             Console.ReadKey();
+        }
+    }
+}
